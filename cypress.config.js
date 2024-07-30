ae4f2dbc36f98a68c3bb0cd0bf564ae031f889bd
janascher <janainascher@protonmail.com>
2024-07-30 09:58:28 -0300

    feat: relatório com mochawasome

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'https://www.automationpratice.com.br/',
-    defaultCommandTimeout: 5000, // Timeout padrão do projeto todo
-    // viewportHeight: 335,
-    // viewportWidth: 999,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    e2e: {
+        reporter: 'cypress-mochawesome-reporter',
+        reporterOptions: {
+            charts: true,
+            reportPageTitle: 'Projeto do curso de Cypress',
+            embeddedScreenshots: true,
+            inlineAssets: true,
+            saveAllAttempts: false,
+        },
+        baseUrl: 'https://www.automationpratice.com.br/',
+        defaultCommandTimeout: 5000, // Timeout padrão do projeto todo
+        // viewportHeight: 335,
+        // viewportWidth: 999,
+        setupNodeEvents(on, config) {
+            require('cypress-mochawesome-reporter/plugin')(on);
+        },
     },
-  },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,9 +14,10 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
-import './home_page_commands'
-import './register_page_commands'
+import './commands';
+import './home_page_commands';
+import './register_page_commands';
+import 'cypress-mochawesome-reporter/register';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
